62c96862b766eb0bbe094748e0c5f767a3daa092
Tianxiao Wang <xiaoyuhen1994@gmail.com>
2019-03-26 20:17:20 +0800

    Revert "fix: standalone build (#32)" (#40)
    This reverts commit 359af257a3cf84acae033224384f203f56ced233.

--- packages/griffith-standalone/src/index.js
+++ packages/griffith-standalone/src/index.js
@@ -3,21 +3,13 @@ import ReactDOM from 'react-dom'
 import Player from 'griffith'
 
 export function createPlayer(target) {
-  function render(props) {
-    ReactDOM.render(<Player {...props} />, target)
-  }
-
-  render()
-
   return {
     render(props) {
-      render(props)
-      return this
+      ReactDOM.render(<Player {...props} />, target)
     },
 
     dispose() {
-      ReactDOM.render(null, target)
-      return this
+      ReactDOM.unmountComponentAtNode(target)
     },
   }
 }

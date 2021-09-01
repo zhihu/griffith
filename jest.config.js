8341b843963b34ef22be2fa5df18a45281ecb9a3
ambar <ambar.lee@gmail.com>
2021-09-02 07:21:54 +0800

    chore: updaet jest 27, use jest projects

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,6 @@
+const base = require('./jest.config.base')
+
 module.exports = {
-  roots: ['<rootDir>/packages'],
-  collectCoverageFrom: ['packages/**/src/**/*.js'],
-  coveragePathIgnorePatterns: [
-    'webpack.config.js',
-    '.babelrc.js',
-    'constants',
-    'index.js',
-    'styles.js',
-    'constants.js',
-    'icons',
-  ],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '<rootDir>/packages/[^/]+?/(?!src/)',
-    '[^/]+?/__mocks__',
-  ],
-  setupFiles: ['./jest.setup.js'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
+  ...base,
+  projects: ['packages/*'],
 }
---
+++ packages/griffith-utils/jest.config.js
@@ -0,0 +1,7 @@
+const base = require('../../jest.config.base')
+
+module.exports = {
+  ...base,
+  testEnvironment: 'node',
+  preset: 'es-jest',
+}

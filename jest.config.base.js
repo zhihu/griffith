90b81662e6ec52e72abc9aa589599def10c49aa8
ambar <ambar.lee@gmail.com>
2021-09-07 16:09:03 +0800

    test: add test for griffith-message

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,6 +1,10 @@
 module.exports = {
   setupFiles: [require.resolve('./jest.setup.js')],
   snapshotSerializers: ['enzyme-to-json/serializer'],
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    // 约定：入口文件只包含 import/export，不执行逻辑
+    '!src/index.{js,jsx,ts,tsx}',
+  ],
   testPathIgnorePatterns: ['/__mocks__/'],
 }
---
+++ packages/griffith-message/src/__test__/createMessageHelper.spec.ts
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import createMessageHelper from '../createMessageHelper'
+
+const waitMessage = () => new Promise(r => setTimeout(r))
+
+test('calls helper', async () => {
+  const helper = createMessageHelper('myId')
+  const handler = jest.fn()
+  const disposer = helper.subscribeMessage(handler)
+  const type = 'foo'
+  const data = {foo: 1}
+  helper.dispatchMessage(window, type, data)
+  await waitMessage()
+  expect(handler).toHaveBeenCalledWith(type, data, null)
+
+  disposer.unsubscribe()
+  helper.dispatchMessage(window, type, data)
+  await waitMessage()
+  expect(handler).toHaveBeenCalledTimes(1)
+})

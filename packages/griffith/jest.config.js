9ab06d84e5bb842239f65baafeb51ae1f8412b46
ambar <ambar.lee@gmail.com>
2021-09-08 20:08:41 +0800

    test: update snapshots

--- packages/griffith/jest.config.js
+++ packages/griffith/jest.config.js
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const base = require('../../jest.config.base')
 
 module.exports = {
   ...base,
   testEnvironment: 'jsdom',
+  preset: 'es-jest',
 }

d9c0641f1d654e68fd83511443185c5797853555
ambar <ambar.lee@gmail.com>
2021-09-08 20:51:32 +0800

    chore: use vite alias

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,9 +1,33 @@
+/* eslint-disable */
+import path from 'path'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+
+const packages = [
+  'griffith',
+  'griffith-message',
+  'griffith-utils',
+  // TODO: ts-migrate
+  // 'griffith-hls',
+  // 'griffith-mp4',
+]
+
+const packagesAliases = Object.fromEntries(
+  packages.map((x) => {
+    const pkgFile = require.resolve(`${x}/package.json`)
+    const file = path.resolve(path.dirname(pkgFile), require(pkgFile).source)
+    return [x, file]
+  })
+)
 
 /**
  * @see https://vitejs.dev/config/
  * @type {import('vite').UserConfig}
  */
 export default {
+  resolve: {
+    alias: {
+      ...packagesAliases,
+    },
+  },
   plugins: [reactRefresh()],
 }

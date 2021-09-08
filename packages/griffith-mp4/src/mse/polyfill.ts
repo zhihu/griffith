90c791b4688d207389bc973e1445cfaf472922af
ambar <ambar.lee@gmail.com>
2021-09-08 21:46:44 +0800

    chore: update types

--- packages/griffith-mp4/src/mse/polyfill.ts
+++ packages/griffith-mp4/src/mse/polyfill.ts
@@ -1,8 +1,10 @@
 // copy from https://github.com/google/shaka-player/blob/master/lib/polyfill/mediasource.js#L125
 function abortPolyfill() {
+  // eslint-disable-next-line @typescript-eslint/unbound-method
   const addSourceBuffer = MediaSource.prototype.addSourceBuffer
   MediaSource.prototype.addSourceBuffer = function (...varArgs) {
     const sourceBuffer = addSourceBuffer.apply(this, varArgs)
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     sourceBuffer.abort = function () {} // Stub out for buggy implementations.
     return sourceBuffer
   }

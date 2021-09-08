cc1dc410df8df1e59d9b2e43f24b5bb4ee60421d
ambar <ambar.lee@gmail.com>
2021-09-08 20:04:47 +0800

    chore: update types in utils

--- packages/griffith-utils/src/__tests__/ua.spec.ts
+++ packages/griffith-utils/src/__tests__/ua.spec.ts
@@ -7,6 +7,7 @@ test('ua', () => {
     value:
       'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Mobile Safari/537.36',
   })
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const {isMobile, isAndroid, isSafari} = require('../ua')
   expect(isMobile).toBe(true)
   expect(isAndroid).toBe(true)
--- packages/griffith-utils/src/sequence.ts
+++ packages/griffith-utils/src/sequence.ts
@@ -1,3 +1,6 @@
-export default function sequence(...fns: any[]) {
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+type AnyFunction = (...args: any[]) => any
+
+export default function sequence(...fns: AnyFunction[]) {
   return (...args: any[]) => fns.reduce((_, fn) => fn(...args), null)
 }

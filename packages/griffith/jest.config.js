be8c23a83200eeb86ec7240dbc0a2a04ed8d230d
ambar <ambar.lee@gmail.com>
2021-09-10 15:50:39 +0800

    chore: use es-jest by default

--- packages/griffith/jest.config.js
+++ packages/griffith/jest.config.js
@@ -1,8 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const base = require('../../jest.config.base')
 
 module.exports = {
   ...base,
   testEnvironment: 'jsdom',
-  preset: 'es-jest',
 }

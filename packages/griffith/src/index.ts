7eea5b02bc4833f44ca065ebb078bf6a6a0b48fa
ambar <ambar.lee@gmail.com>
2021-09-15 17:46:26 +0800

    chore: rename variables in MessageContext

--- packages/griffith/src/index.ts
+++ packages/griffith/src/index.ts
@@ -1,9 +1,6 @@
 export {default} from './components/Player'
 export * from './components/Player'
-export {
-  ExternalContext as MessageContext,
-  useMessageContextRef,
-} from './contexts/MessageContext'
+export {MessageContext, useMessageContextRef} from './contexts/MessageContext'
 export {default as VideoSourceContext} from './contexts/VideoSourceContext'
 export {default as Controller} from './components/Controller'
 export {default as Layer} from './components/Layer'

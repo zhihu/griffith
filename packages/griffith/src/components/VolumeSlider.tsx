aa8242a54e7c74afd382847dcf4e1aa40c6996fd
ambar <ambar.lee@gmail.com>
2021-09-17 22:21:03 +0800

    refactor: prefer function component

--- packages/griffith/src/components/VolumeSlider.tsx
+++ packages/griffith/src/components/VolumeSlider.tsx
@@ -1,23 +1,23 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {css} from 'aphrodite/no-important'
 import Slider, {SliderProps} from './Slider'
 import styles, {slider as sliderStyles} from './VolumeSlider.styles'
 
 type DefinedProps = Pick<SliderProps, 'styles' | 'orientation' | 'step'>
 
-class VolumeSlider extends Component<Omit<SliderProps, keyof DefinedProps>> {
-  render() {
-    return (
-      <div className={css(styles.root)}>
-        <Slider
-          {...this.props}
-          styles={sliderStyles}
-          orientation="vertical"
-          step={0.1}
-        />
-      </div>
-    )
-  }
+const VolumeSlider: React.FC<Omit<SliderProps, keyof DefinedProps>> = (
+  props
+) => {
+  return (
+    <div className={css(styles.root)}>
+      <Slider
+        {...props}
+        styles={sliderStyles}
+        orientation="vertical"
+        step={0.1}
+      />
+    </div>
+  )
 }
 
 export default VolumeSlider

924bec766023c68fe592fbae63ae1f2f24c48c4c
ambar <ambar.lee@gmail.com>
2021-09-22 13:58:36 +0800

    refactor: use hooks in Timeline, add useBoolean

--- packages/griffith/src/components/items/TimelineItem.tsx
+++ packages/griffith/src/components/items/TimelineItem.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import {css} from 'aphrodite/no-important'
 import styles from '../Controller.styles'
-import Timeline from '../Timeline'
+import Timeline, {TimelineProps} from '../Timeline'
 
-const TimelineItem = (props: any) => {
+const TimelineItem = (props: TimelineProps) => {
   return (
     <div className={css(styles.timeline)}>
       <Timeline {...props} />

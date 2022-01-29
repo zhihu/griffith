0bb4463d532b378a5231a4d64e29c936112079be
ambar <ambar.lee@gmail.com>
2022-01-29 14:27:44 +0800

    fix: add isIE in ua
    #18 #158

--- packages/griffith-utils/src/__tests__/ua.spec.ts
+++ packages/griffith-utils/src/__tests__/ua.spec.ts
@@ -2,14 +2,30 @@
  * @jest-environment jsdom
  */
 
+import ua, {parseUA} from '../ua'
+
 test('ua', () => {
-  Object.defineProperty(window.navigator, 'userAgent', {
-    value:
-      'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Mobile Safari/537.36',
-  })
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const {isMobile, isAndroid, isSafari} = require('../ua')
-  expect(isMobile).toBe(true)
-  expect(isAndroid).toBe(true)
-  expect(isSafari).toBe(false)
+  expect(ua).toMatchInlineSnapshot(`
+    Object {
+      "isAndroid": false,
+      "isIE": false,
+      "isMobile": false,
+      "isSafari": false,
+    }
+  `)
+})
+
+test('parse', () => {
+  expect(
+    parseUA(
+      'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Mobile Safari/537.36'
+    )
+  ).toMatchInlineSnapshot(`
+    Object {
+      "isAndroid": true,
+      "isIE": false,
+      "isMobile": true,
+      "isSafari": false,
+    }
+  `)
 })
--- packages/griffith/src/utils/parseUA.ts
+++ packages/griffith/src/utils/parseUA.ts
@@ -1,4 +1,4 @@
-export function parseUA(userAgent: string) {
+export default function parseUA(userAgent: string) {
   return {
     isIE: /MSIE|Trident/i.test(userAgent),
     isMobile: /iPhone|iPad|iPod|Android/i.test(userAgent),
@@ -6,8 +6,3 @@ export function parseUA(userAgent: string) {
     isSafari: /^((?!chrome|android).)*safari/i.test(userAgent),
   }
 }
-
-export default parseUA(
-  // TODO: 加一个 context 让各处访问更好
-  typeof navigator !== 'undefined' ? navigator.userAgent : ''
-)

0d245898a29fb4ac0f2972edb3fdc6b12ccb2bcf
ambar <ambar.lee@gmail.com>
2024-03-18 15:37:48 +0800

    chore: remove unused website files

--- website/src/components/CodeBlock/CodeBlock.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CodeBlock = ({children}) => (
-  <pre>
-    <code>{children}</code>
-  </pre>
-)
-
-export default CodeBlock
--- website/src/components/MainContent/MainContent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const MainContent = ({children}) => (
-  <main className="main-content">{children}</main>
-)
-
-export default MainContent
